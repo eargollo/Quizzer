cd7fa5eb80385f6622ea38e38ab6e19e2d7553ff
Eduardo Argollo <eargollo@gmail.com>
2012-01-28 23:31:24 -0200

    Added a cleaner for creating the jRuby releases

---
+++ tmp/jquizzer_clean.rb
@@ -0,0 +1,36 @@
+clean_list = [
+  "bin/quizzer",
+  "bin/quizzer.bat",
+  "bin/quizzer_red.bat",
+  "lib/quizzer/view",
+  "lib/quizzer/view_red_shoes",
+  "lib/quizzer/view_green_shoes.rb",
+  "lib/quizzer/view_red_shoes.rb",
+  "lib/quizzer_jruby.rb",
+  "lib/quizzer_green.rb",
+  "lib/quizzer_red.rb",
+  "tmp",
+  "script",
+  "test",
+  "Rakefile",
+  "Manifest.txt",
+  "PostInstall.txt"
+]
+
+
+require "fileutils"
+base_path = File.expand_path(File.dirname(__FILE__) + "/..")
+
+puts base_path
+
+clean_list.each do |file|
+  extended_name = "#{base_path}/#{file}"
+  raise "File #{extended_name} does not exist" if !File.exists?(extended_name)
+  if File.directory?(extended_name)
+  `git rm #{extended_name} -r`
+  #  FileUtils.rm_r(extended_name)
+  else
+  #  FileUtils.rm(extended_name)
+    `git rm #{extended_name}`
+  end
+end
